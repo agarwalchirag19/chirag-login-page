364966b05028c1e5835b461a28dfea40a1bbc015
agarwalchirag19 <botxchirag@gmail.com>
2022-11-15 13:07:13 +0530

    login-page

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div>
+            logo
+        </div>
+        <div>
+            <ul>
+                <li>contact</li>
+                <li>sevices</li>
+            </ul>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ connectMongo.js
@@ -0,0 +1,3 @@
+import mongoose from 'mongoose'
+
+const connectMongo = async () => mongoose.connect()
